74c9051d41a1177cac516a45e55702308b915bc6
Michael W. Johnson <digitlworld@gmail.com>
2024-06-03 17:50:24 -0400

    Generates all flower recipes

--- src/main/java/com/example/examplemod/MoreInsolationMod.java
+++ src/main/java/com/example/examplemod/MoreInsolationMod.java
@@ -1,43 +1,26 @@
 package com.example.examplemod;
 
-import cofh.core.init.data.providers.CoreTagsProvider;
-import cofh.lib.common.fluid.FluidCoFH;
 import cofh.lib.common.fluid.FluidIngredient;
-import cofh.thermal.core.util.managers.machine.InsolatorRecipeManager;
 import cofh.thermal.core.util.recipes.machine.InsolatorRecipe;
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.telemetry.events.WorldLoadEvent;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.Container;
-import net.minecraft.world.item.BlockItem;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.Recipe;
-import net.minecraft.world.level.block.SaplingBlock;
-import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerAboutToStartEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-//import com.teamcofh.
 import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -63,22 +46,43 @@ public class MoreInsolationMod
     public void onLoadComplete(ServerAboutToStartEvent event) {
 
         var rm = event.getServer().getRecipeManager();
-        ResourceLocation id = new ResourceLocation(MoreInsolationMod.MODID, "test_recipe");
-
-        Ingredient inputItem = Ingredient.of(Items.DIAMOND);
-
-        FluidStack water = new FluidStack(Fluids.WATER, 1000);
-        FluidIngredient waterIngredient = FluidIngredient.of(water);
-
-        ItemStack outputItem = new ItemStack(Items.DIAMOND, 1);
-        Float outputItemChance = 2.0f;
-
-        InsolatorRecipe recipe = new InsolatorRecipe(id, 3000, 3.0f, List.of(inputItem), List.of(waterIngredient), List.of(outputItem), List.of(2.0f), List.of() );
 
         var curRecipes = rm.getRecipes();
-        curRecipes.add(recipe);
+        curRecipes.addAll(generateFlowerRecipes());
         rm.replaceRecipes(curRecipes);
+    }
 
+    private List<InsolatorRecipe> generateFlowerRecipes()
+    {
+        var tagkey = ItemTags.create(new ResourceLocation("minecraft", "flowers"));
+
+        var tag = ForgeRegistries.ITEMS.tags().getTag(tagkey);
+
+        return tag.stream().map(this::generateRecipeForFlower).toList();
+    }
+
+    private InsolatorRecipe generateRecipeForFlower( Item flower )
+    {
+        var itemLocation = ForgeRegistries.ITEMS.getKey(flower);
+
+        if( itemLocation != null )
+        {
+            var recipeName = "insolation_recipe/" + itemLocation.getNamespace() + "/" + itemLocation.getPath();
+
+            ResourceLocation id = new ResourceLocation(MoreInsolationMod.MODID, recipeName);
+
+            Ingredient inputItem = Ingredient.of(flower);
+
+            FluidStack water = new FluidStack(Fluids.WATER, 1000);
+            FluidIngredient waterIngredient = FluidIngredient.of(water);
+
+            ItemStack outputItem = new ItemStack(flower, 1);
+            Float outputItemChance = 2.0f;
+
+            return new InsolatorRecipe(id, 20000, 3.0f, List.of(inputItem), List.of(waterIngredient), List.of(outputItem), List.of(outputItemChance), List.of());
+        }
+
+        return null;
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
